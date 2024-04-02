838ba4745e7c779980fb6e67a63ecfdc0fb015aa
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-04-02 11:14:54 +0300

    change delete by owner

--- src/index.js
+++ src/index.js
@@ -25,6 +25,26 @@ app.get("/todos", (req, res) => {
     res.status(404).json("error");
   }
 });
+app.get("/owners", (req, res) => {
+  const limit = req.query.limit ?? 2;
+  const ownerID = req.query.statusTodo;
+  let filterOwners = owners;
+  if (ownerID) {
+    // Your Endpoint URL: http://localhost:1463/todos/?statusTodo=todo
+    http: filterOwners = filterOwners.filter(
+      (owner) => owner.ownerID === ownerID
+    );
+  }
+  if (filterOwners) {
+    //Your Endpoint URL: http://localhost:1463/todos/?limit=10
+    http: filterOwners = filterOwners.slice(0, limit);
+  }
+  if (filterOwners.length > 0) {
+    res.status(200).json(filterOwners);
+  } else {
+    res.status(404).json("error");
+  }
+});
 app.get("/todos/:id", (req, res) => {
   const { id } = req.params;
   let todo;
@@ -33,7 +53,7 @@ app.get("/todos/:id", (req, res) => {
     res.send(todo);
   }
 });
-//Your Endpoint URL: http://localhost:1463/todos/owner/a1
+//Your Endpoint URL: http://localhost:1463/todos/owner/a2
 http: app.get("/todos/owner/:ownerId", (req, res) => {
   const ownerId = req.params.ownerId;
   const owner = owners.find((ow) => ow.id === ownerId);
@@ -91,18 +111,71 @@ app.put("/todos/:id", (req, res) => {
     res.status(404).send({ message: "there is no todo with that id", id });
   }
 });
-//Your Endpoint URL : http://localhost:1463/todos/changeStatus/1/done
-app.put("/todos/changeStatus/:id/:statusTodo", (req, res) => {
+//Your Endpoint URL : http://localhost:1463/changeStatus/1/status
+app.put("/changeStatus/:id/status", (req, res) => {
   const { id } = req.params;
-  const { statusTodo } = req.body;
-  const todoToUpdate = todos.find((item) => item.id === id);
-  if (todoToUpdate) {
-    todoToUpdate.statusTodo = statusTodo;
-    res.status(200).json({
-      message: `Todo with ID ${id} has been updated with status '${statusTodo}'.`,
+  let updated = false;
+
+  todos = todos.map((todo) => {
+    if (todo.id === id) {
+      updated = true;
+
+      return {
+        ...todo,
+        statusTodo: req.body.statusTodo ? req.body.statusTodo : todo.statusTodo,
+      };
+    } else {
+      return todo;
+    }
+  });
+
+  if (updated) {
+    res.status(200).send({
+      message: `${
+        req.body.statusTodo
+          ? "todo status is updated"
+          : "todo status is the same"
+      }`,
+      id,
     });
+  } else {
+    res.status(404).send({ message: "there is no todo with that id", id });
   }
-  res.status(404).json({ message: "No todo found with that id" });
+});
+
+//Your Endpoint URL :http://localhost:1463/todos/changeOwner/a2/newOwnerId
+//owner-in id-sini deyisirik
+app.put("/todos/changeOwner/:id/newOwnerId", (req, res) => {
+  const { id } = req.params;
+  let update = false;
+  owners = owners.map((item) => {
+    if (item.id === id) {
+      update = true;
+      return { ...item, id: req.body.id ? req.body.id : item.id };
+    } else {
+      return item;
+    }
+  });
+
+  todos = todos.map((todo) => {
+    if (todo.ownerId === id) {
+      update = true;
+      return { ...todo, ownerId: req.body.id };
+    } else {
+      return todo;
+    }
+  });
+
+  if (update) {
+    res.status(200).send({
+      message: `${
+        req.body.id ? "owner id is updated" : "owner id is the same"
+      }`,
+      id,
+    });
+  } else {
+    res.status(404).send({ message: "there is no owner with that id", id });
+  }
 });
 
 //Your Endpoint URL : http://localhost:1463/todos/1
@@ -113,17 +186,41 @@ app.delete("/todos/:id", (req, res) => {
 });
 //Your Endpoint URL : http://localhost:1463/todos/status/todo
 app.delete("/todos/status/:statusTodo", (req, res) => {
-  const { statusTodo } = req.body;
-  todos = todos.filter((todo) => todo.statusTodo !== statusTodo);
-  res
-    .status(200)
-    .json({ message: `Todos with status ${statusTodo} are deleted` });
+  const { statusTodo } = req.params;
+  const todosDelet = todos.filter((todo) => todo.statusTodo === statusTodo);
+  if (todosDelet.length > 0) {
+    todos = todos.filter((todo) => todo.statusTodo !== statusTodo);
+    res
+      .status(200)
+      .json({ message: `Todos with status ${statusTodo} are deleted` });
+  } else {
+    res
+      .status(404)
+      .json({ message: `Todos of status ${statusTodo} are not found` });
+  }
 });
-//Your Endpoint URL : http://localhost:1463/todos/status/a2
-app.delete("/todos/status/:owerId", (req, res) => {
-  const { ownerId } = req.body;
-  todos = todos.filter((todo) => todo.ownerId !== ownerId);
-  res.status(200).json({ message: `Todos with status ${ownerId} are deleted` });
+//Your Endpoint URL : http://localhost:1463/todos/delete/a1
+app.delete("/todos/delete/:ownerId", (req, res) => {
+  const { ownerId } = req.params;
+  const ownerFoundByTodoId = owners.find((owner) => owner.id === ownerId);
+
+  if (ownerFoundByTodoId) {
+    const deletedTodos = todos.filter((todo) => todo.ownerId === ownerId);
+    todos = todos.filter((todo) => todo.ownerId !== ownerId);
+
+    ownerFoundByTodoId.todos = "no todos";
+
+    deletedTodos.length > 0
+      ? res.status(200).json({
+          message: `All todos by owner with id ${ownerId} are deleted`,
+          deletedTodos: deletedTodos,
+        })
+      : res.status(200).json({
+          message: `There are no todos`,
+        });
+  } else {
+    res.status(404).json({ message: `There are no owners with id ${ownerId}` });
+  }
 });
 
 app.listen(portTodo, () => {
